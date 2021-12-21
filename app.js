c022263abcb3cad8e805fe50fa317308d45ff324
Kia Boluki <Kia.Boluki@gmail.com>
2021-12-22 02:12:21 +0330

    change the image resource to picsum.photo

--- app.js
+++ app.js
@@ -1,60 +1,57 @@
 
 
 const body = document.querySelector('body');
-const pixabayLogo = document.querySelector('.pixabay-logo');
-const mainImg = document.querySelector('.main-image');
-const pixabayLink = document.querySelector('.pixabay-logo a');
+// const pixabayLogo = document.querySelector('.pixabay-logo');
+// const mainImg = document.querySelector('.main-image');
+// const pixabayLink = document.querySelector('.pixabay-logo a');
 const lblGregorianDate = document.getElementById('gregorian-date');
 const lblPersianDate = document.getElementById('persian-date');
 const lblTime = document.getElementById('time');
 const lblPartOfDay = document.getElementById('part-of-day');
 
-const apiURL = new URL('https://pixabay.com/api?key=23746636-55ee33f792fb23acfe5b7eff5');
+// const apiURL = new URL('https://pixabay.com/api?key=23746636-55ee33f792fb23acfe5b7eff5');
 
-const params = {
-  q: '',
-  image_type: 'photo',
-  orientation: 'horizontal',
-  category: 'backgrounds',
-  min_width: 1080,
-  colors: "blue,green,red,yellow,orange,purple,pink,brown,grey,black",
-  editors_choice: true,
-  safesearch: true,
-  order: 'popular',
-}
+// const params = {
+//   q: '',
+//   image_type: 'photo',
+//   orientation: 'horizontal',
+//   category: 'backgrounds',
+//   min_width: 1080,
+//   colors: "blue,green,red,yellow,orange,purple,pink,brown,grey,black",
+//   editors_choice: true,
+//   safesearch: true,
+//   order: 'popular',
+// }
 
-apiURL.search = new URLSearchParams(params).toString();
+// apiURL.search = new URLSearchParams(params).toString();
 
-// get body element 
-// fetch images and save them in localstorage
-if (localStorage.getItem('kia-theme-pixabay-hits') === null) {
-  fetch(apiURL)
-    .then(response => {
-      return response.json();
-    }).then(data => {
+// fetch(apiURL, {
+//   method: 'GET',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     "allow-control-allow-origin": "*",
+//   }
+// })
+//   .then(response => {
+// return response.json();
 
-      localStorage.setItem('kia-theme-pixabay-hits', JSON.stringify(data.hits));
-    }).catch(error => {
-      console.log(error);
-    });
-}
-
-
-const hits = JSON.parse(localStorage.getItem('kia-theme-pixabay-hits'));
-console.log(hits);
-// set body background image to random image from hits.webformatURL
-const randomImage = hits[Math.floor(Math.random() * hits.length)];
-console.log(randomImage);
-
-const imgUrl = randomImage.largeImageURL;
-const imgPixabayURL = randomImage.pageURL;
-// get hits from localstorage
-
-
-body.style.backgroundImage = `url(${imgUrl})`;
-mainImg.style.backgroundImage = `url(${imgUrl})`;
-
-pixabayLink.href = imgPixabayURL;
+//   })
+//   .then(data => {
+//     if ( data.hits != null && data.hits.length > 0) {
+     
+//       const randomIndex = Math.floor(Math.random() * data.hits.length);
+//       const randomImage = data.hits[randomIndex];
+      
+//       const imgUrl = randomImage.largeImageURL;
+//       const imgPixabayURL = randomImage.pageURL;
+  
+//       body.style.backgroundImage = `url(${imgUrl})`;
+//       mainImg.style.backgroundImage = `url(${imgUrl})`;
+      
+//       pixabayLink.href = imgPixabayURL;
+//     }
+//   })
+//   .catch(error => console.log(error));
 
 const getDateTime = (locale = 'default' ) => {
   const today = new Date();
@@ -99,10 +96,7 @@ setInterval(() => {
   ${getDateTime().day}, 
   ${getDateTime().year} `;
   
-  lblTime.textContent = `
-  ${( getDateTime().Hour < 10 ) ? '0' + getDateTime().Hour :  getDateTime().Hour }:
-  ${( getDateTime().Minutes < 10 ) ? '0' + getDateTime().Minutes :  getDateTime().Minutes }:
-  ${( getDateTime().Seconds < 10 ) ? '0' + getDateTime().Seconds :  getDateTime().Seconds } `;
+  lblTime.textContent = `${( getDateTime().Hour < 10 ) ? '0' + getDateTime().Hour:getDateTime().Hour }:${( getDateTime().Minutes < 10 ) ? '0' + getDateTime().Minutes:getDateTime().Minutes }:${( getDateTime().Seconds < 10 ) ? '0' + getDateTime().Seconds:getDateTime().Seconds }`;
 
   lblPersianDate.textContent = `
   ${getDateTime('fa-IR').weekDay},
